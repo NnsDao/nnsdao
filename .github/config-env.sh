897c4e5961458a5dee1d44e1a513a38e6807430d
neptune0xf <null>
2022-07-11 02:54:17 +0000

    🔄 Created local '.github/config-env.sh' from remote 'workflows/config-env.sh'

---
+++ .github/config-env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DFX_VERSION=$(node -p "require('./dfx.json').dfx")
+
+echo "Install dfx,version: ${DFX_VERSION}"
+# //  Use the DFX_VERSION environment variable to identify a specific version of the SDK that you want to install.
+DFX_VERSION=${DFX_VERSION} sh -ci "$(curl -fsSL https://smartcontracts.org/install.sh)"
+
+echo "Install global npm package"
+
+if [ -f "./package.json" ]; then
+  echo "File \"./package.json\" exists"
+else
+  echo 'Init npm project config'
+  npm init -y
+fi
+
+npm i @dfinity/agent @dfinity/principal @dfinity/candid @dfinity/identity glob
+# npm list -g --depth=0
