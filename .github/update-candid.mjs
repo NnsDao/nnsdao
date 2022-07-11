0502e2c323a123874ac76eeabec7639236e6d53b
neptune0xf <null>
2022-07-11 03:29:34 +0000

    🔄 Synced local '.github/update-candid.mjs' with remote 'workflows/backend/update-candid.mjs'

--- .github/update-candid.mjs
+++ .github/update-candid.mjs
@@ -1,6 +1,5 @@
 import { Actor, HttpAgent } from '@dfinity/agent';
 import { readFileSync, writeFileSync } from 'fs';
-import glob from 'glob';
 import { resolve } from 'path';
 
 const idlFactory = ({ IDL }) =>
@@ -12,19 +11,23 @@ const onlineHost = 'https://ic0.app';
 const anonymousAgent = new HttpAgent({ host: onlineHost });
 
 const canister_ids = JSON.parse(readFileSync(relativeToRootPath('canister_ids.json')).toString());
+const dfxJson = JSON.parse(readFileSync(relativeToRootPath('dfx.json')).toString());
 
 Object.keys(canister_ids).forEach(async canisterName => {
   const cid = canister_ids[canisterName].ic;
-  const candidStr = await getCandid(cid);
-  const isRustProject = glob.sync('*.toml', { cwd: resolve('./') });
-  if (isRustProject.length) {
-    try {
-      // @ts-ignore
-      writeFileSync(relativeToRootPath(`./src/${canisterName}/${canisterName}.did`), candidStr);
-    } catch (error) {
-      // if canister_ids config multiple canisterId,such as test,prod,xxx,ignore this error
-      console.error('error', error);
+  const candidPath = dfxJson.canisters[canisterName].candid;
+  try {
+    const candidStr = await getCandid(cid);
+    if (!candidStr) {
+      console.warn('candid Not found');
+      process.exit(1);
     }
+
+    // @ts-ignore
+    writeFileSync(relativeToRootPath(`${candidPath}`), candidStr);
+  } catch (error) {
+    // if canister_ids config multiple canisterId,such as test,prod,xxx,ignore this error
+    console.error('error', error);
   }
 });
 
