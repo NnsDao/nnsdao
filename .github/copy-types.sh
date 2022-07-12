8885abd55b260ab99d7359a6c47a64ca1ad1c33a
neptune0xf <null>
2022-07-12 09:40:45 +0000

    🔄 Synced file(s) with NnsDao/nnsdao-actions

--- .github/copy-types.sh
+++ .github/copy-types.sh
@@ -3,11 +3,13 @@
 # Make sure path ends with /
 for directory in '.dfx/local/canisters/'*; do
   if [[ -d "${directory}" && ! -L "${directory}" ]]; then
+
     if [ -f "${directory}/index.js" ]; then
+      echo "${directory}"
       # rename xxx/xxx.did.js  to xxx/xxx.ts
-      mv ${directory}/${directory}.did.js ${directory}/${directory}.did.ts
+      mv ${directory}/*.did.js ${directory}/index.did.ts || true
       # rename xxx.did.d.ts  to types.ts
-      mv ${directory}/${directory}.did.d.ts ${directory}/types.ts
+      mv ${directory}/*.did.d.ts ${directory}/types.ts || true
     fi
   fi
 done
