3e86b59e8d242cb7eae970e4b185cd14f4890ed1
Milo <cwb2819259@gmail.com>
2022-09-05 13:03:04 +0800

    feat: change owner set

--- src/nnsdao/src/init.rs
+++ src/nnsdao/src/init.rs
@@ -4,9 +4,9 @@ use ic_kit::ic;
 
 #[init]
 
-fn init() {
+fn init(owner: Principal) {
     ic_cdk::setup();
     let data = ic::get_mut::<Data>();
-    data.owners.add_owner(ic_cdk::caller());
+    data.owners.add_owner(owner);
     // data.dao.update_dao_info(dao_info).unwrap();
 }
